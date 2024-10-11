cc36fc04fabf207182ac49da85f46bb6d506d9e7
Dnica98 <daniela.nica98@gmail.com>
2024-10-11 17:29:29 +0300

    1

---
+++ index.js
@@ -0,0 +1,76 @@
+
+
+// const arr = [1,2,3,4,5,6]
+// const arr1 = arr.map((item,index) => {
+//     return item + 2;
+// })
+
+// console.log('ex1', arr1)
+
+// const arr2 =arr.map((x,index) =>{
+//     console.log(index);
+//     return x + index;
+// });
+
+// console.log('ex2', arr2)
+
+// const arr3 = arr.map((item, index) => {
+//     return item *2;
+// })
+
+// console.log('ex3', arr3)
+
+// const arr4 = arr.map((item,index) => {
+//     return item / 2;
+// })
+
+// console.log('ex4', arr4)
+
+// const arr5 = arr.map((item) => Math.pow(item, 2))
+
+// console.log('ex5', arr5)
+
+const matrice = ['abcd','bcde','cdef','defh','efhg']
+
+// const matrice1 = matrice.map((item, index) =>{
+//     return `For ${item} is index nr ${index}`;
+// }) ;
+
+// console.log('ex1', matrice1)
+
+// const matrice2 = matrice.map((index,item) =>{
+//     return index + item;
+// })
+
+// console.log('ex2', matrice2)
+
+// const matrice3 =matrice.map((item) =>{
+//     return item[0];
+// });
+
+// console.log('ex3', matrice3)
+
+
+// const matrice4 = matrice.map((item) =>{
+//     return item[-1];
+// });
+
+// console.log('ex4', matrice4)
+
+
+// const copie = [1,2,3,4,5]
+// const copie1=[...copie,...copie]
+
+// console.log(copie1)
+
+
+// const keyTest = {
+//     key1: 'fereastra',
+//     key2: 'balcon',
+//     key3: 'bucatarie',
+//     key4: 'dormitor'
+
+// }
+
+// const keyTest1 = {...keyTest, ...keyTest, key3:'comoda'}
+// console.log(keyTest1)
